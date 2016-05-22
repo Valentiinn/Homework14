b8cb1d2c0643643375441623839faa04e0d20f46
Valentin Karayvansky <Valik-isuzu@mail.ru>
2016-05-22 20:54:50 +0300

    Fix a problem

--- src/com/Karayvansky/Homework14/Player.java
+++ src/com/Karayvansky/Homework14/Player.java
@@ -39,16 +39,18 @@ public abstract class Player {
 		if (obj == null) {
 			return false;
 		}
-		if (obj instanceof Player) {
-			Player player = (Player) obj;
-			if (this.firstName == null || this.getLastName() == null) {
-				return false;
-			}
-			if (this.firstName.equals(player.firstName) && this.getLastName().equals(player.getLastName())
-					&& this.type == player.type) {
-				return true;
-			}
+		if (obj.getClass() != Player.class) {
+			return false;
 		}
+		Player player = (Player) obj;
+		if (this.firstName == null || this.getLastName() == null) {
+			return false;
+		}
+		if (this.firstName.equals(player.firstName) && this.getLastName().equals(player.getLastName())
+				&& this.type == player.type) {
+			return true;
+		}
+
 		return false;
 	}
 
